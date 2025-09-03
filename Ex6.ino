ccf1acd0bb0cb98cefc549ddaaf506df6b8a9bd1
Vishal V <168668286+frankiye04@users.noreply.github.com>
2025-09-03 10:54:49 +0530

    Ex6 iOT

--- Ex6.ino
+++ Ex6.ino
@@ -1,43 +1,38 @@
-Ex6 Coding:
 #include <WiFi.h>
 #include "ThingSpeak.h"
+
 const char* ssid = "Wifi Name";
 const char* password = "Wifi password";
+
 WiFiClient client;
-unsigned long channelID = Channel ID From Thingspeak;
-const char* writeAPIKey = "Write API Key from Thingspeak";
-#define LM35
-PIN A0
-_
-void setup()
-{
-Serial.begin(9600); //Baud rate shown at the top right of Serial Monitor
-WiFi.begin(ssid, password);while (WiFi.status() != WL
-_
-CONNECTED)
-{
-delay(500);
-Serial.print("
-.
-");
+
+unsigned long channelID = Channel_ID_From_Thingspeak;
+const char* writeAPIKey = "Write_API_Key_from_Thingspeak";
+
+#define LM35_PIN A0
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Connected!");
+  ThingSpeak.begin(client);
 }
-Serial.println("\nWiFi Connected!");
-ThingSpeak.begin(client);
-}
+
 void loop() {
-long sum = 0;
-const int samples = 64; // Take 64 readings
-for (int i = 0; i < samples; i++) {
-sum += analogRead(LM35
-_
-PIN);
-delay(2); // Small delay between readings (optional)
-}
-// Calculate average
-float raw = sum / (float)samples;
-float voltage = raw * (5.0 / 1024.0);
-float temperature = voltage * 100.0;
-Serial.println("Temperature: " + String(temperature));
-ThingSpeak.writeField(channelID, 1, temperature, writeAPIKey);
-delay(15000);
+  long sum = 0;
+  const int samples = 64;
+  for (int i = 0; i < samples; i++) {
+    sum += analogRead(LM35_PIN);
+    delay(2);
+  }
+  float raw = sum / (float)samples;
+  float voltage = raw * (5.0 / 1024.0);
+  float temperature = voltage * 100.0;
+  Serial.println("Temperature: " + String(temperature));
+  ThingSpeak.writeField(channelID, 1, temperature, writeAPIKey);
+  delay(15000);
 }
